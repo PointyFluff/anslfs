3a83a0eac987d49676b35f70fe3a2c1360aa598f
PointyFluff <PointyFluff@users.noreply.github.com>
2020-03-03 12:03:14 -0800

    skeletal setup, begin making tools to help

---
+++ tools/new_roll.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# new-roll
+# hot fluffy rolls for ansible. ;)
+# get roll name, make dirs for roll
+# TODO: touch base files.
+
+if [[ -n $1 ]]; then
+	roll_name=$1 #mmm...buttery
+else 
+	echo -n "Enter a name for the Ansible role: "
+	read roll_name
+fi
+
+roll_name="${roll_name//[^a-zA-Z0-9]/}" #sanitize user input
+roll_name="${roll_name,,}" #force lowercase
+
+if [[ -z $roll_name ]]; then 
+	echo "ERROR: Invalid Name"
+	exit 1
+fi
+
+
+if [[ ! -d "$roll_name" ]]; then
+	mkdir -p "./roles/${roll_name}/{tasks,handlers,files,templates,vars,defaults,meta}"
+else
+	echo "Nope: A folder named: ${roll_name} already exists."
+	exit 0
+fi
+
+# COWER, BEEF MORTALS
+#   --Vaca de la Muerte
